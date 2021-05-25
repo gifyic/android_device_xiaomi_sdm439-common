2337b1cefc59c438280b84153a54bcad6440ade1
zqm64 <zqm64@outlook.com>
2021-05-25 09:56:20 +0800

    sdm439-common: prop: Import common camera props from stock

--- properties.mk
+++ properties.mk
@@ -4,6 +4,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.camera.privapp.list=org.codeaurora.snapcam \
+persist.sys.camera.camera2=true \
+persist.vendor.camera.display.umax=1920x1080 \
+persist.vendor.camera.display.lmax=1280x720 \
+persist.vendor.camera.privapp.list=org.codeaurora.snapcam \
+persist.vendor.camera.stats.test=5 \
+vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.qualcomm.qti.qmmi,com.huaqin.factory,com.westalgo.calibration \
+vendor.camera.aux.packagelist1=com.huaqin.cameraautotest,com.xiaomi.cameratools,com.android.camera \
+vendor.camera.aux.packagelist2=com.mi.AutoTest
+
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.sf.lcd_density=320
