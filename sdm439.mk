a1206729e3cc18e0e000e9f575e3f07ac63dfb32
zqm64 <zqm64@outlook.com>
2021-05-29 11:51:12 +0800

    sdm439-common: Grant permission to input devices to QTI Power AIDL
    We're currently setting DT2W via /dev/input interface.

--- sdm439.mk
+++ sdm439.mk
@@ -10,6 +10,10 @@ $(call inherit-product, device/xiaomi/msm8937-common/msm8937.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Power
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power/android.hardware.power-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.power-service-qti.rc
+
 # Properties
 include $(LOCAL_PATH)/properties.mk
 
